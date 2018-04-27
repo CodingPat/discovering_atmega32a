84ad80164921c16ad6eb33edcfc79d12471b3289
woody <woody@tallahassee.local>
2018-04-27 13:48:15 +0200

    USART

--- 03_USART/main.c
+++ 03_USART/main.c
@@ -3,20 +3,29 @@
 // ------- Preamble -------- //
 #include <avr/io.h>                        /* Defines pins, ports, etc */
 #include <util/delay.h>                     /* Functions to waste time */
-#include "../lib/usart.h"
+#include "../toolbox/usart.h"
 
-#define FOSC 1000000
+#define F_CPU 1000000
 #define BAUD 19200
 
 int main(void) {
 	
-
-	USART_init(FOSC,BAUD);
+	char serialCharacter;
+	USART_init(F_CPU,BAUD);
+	USART_printString("Hello World!\r\n");
 								   
   // ------ Event loop ------ //
   while (1) {
+		
+		// get a character from terminal
+		serialCharacter = USART_receiveByte();
 
-    // do nothing
+		// echo character to terminal
+		USART_printString("Character received : ");
+    USART_transmitByte(serialCharacter);
+		USART_printString("\r\n"); /*carriage return + line feed*/
+		
+		
     
   }                                                  /* End event loop */
   return 0;                            
