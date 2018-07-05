becb93a730a1574de190f1987721d0df487b0d31
david <david@prometheus.local>
2018-07-05 19:25:19 +0200

    timer0 ctc mode

--- 06_timer/06_timer.c
+++ 06_timer/06_timer.c
@@ -6,27 +6,32 @@
 
 void init(){
 	
-	//output pin
-	DDRD|=(1<<PD5);//OC1A	
-
-	//Frequence 1hz
-	OCR1A=487;
-	TCCR1B|=(1<<CS12)|(1<<CS10);
-	TCCR1B&=~(1<<CS11);
+	
 	//CTC mode
 	TCCR1B|=(1<<WGM12);
-	TCCR1B&=~(1<<WGM13);
-	TCCR1A&=~(1<<WGM11);
-	TCCR1A&=~(1<<WGM10);
 	
+		
 	//Waveform : toggle logic level	
-	TCCR1A|=(1<<COM1A0);
-	TCCR1A&=~(1<<COM1A1);
+	TCCR1A|=(1<<COM1A0);//pin OC1A
+	//TCCR1A|=(1<<COM1B0);//pin OC1B
 	
+	//prescale clock /1024
+	TCCR1B|=(1<<CS10);
+	TCCR1B|=(1<<CS12);
+	
+	// counter top value
+	OCR1A=487;
+
+	//output pin
+	DDRD|=(1<<PD5);//OC1A	
+	//DDRD|=(1<<PD4);//OC1B	
+
 
 }
 
 int main(){
+
+	init();
 
 	while(1){}
 
